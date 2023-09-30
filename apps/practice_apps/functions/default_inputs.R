925e5f5370422b063029d321876c9fa4eb309c7a
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-09-30 14:14:21 -0400

    wip: test inputs with 'multiply'

--- apps/practice_apps/functions/default_inputs.R
+++ apps/practice_apps/functions/default_inputs.R
@@ -8,7 +8,8 @@ get_inputs <- function(...) {
         paid_new_campaign_list             = c("linkedin", "spotify"),
         paid_new_clicks                    = c(1000, 2000),
         paid_new_leads                     = c(75, 1500),
-        paid_new_cost                      = c(6000, 17000)
+        paid_new_cost                      = c(6000, 17000),
+        multiply = c(0.75, 1.10)
     )
     
     # args list
@@ -18,16 +19,84 @@ get_inputs <- function(...) {
     paid_new_clicks = args$paid_new_clicks
     paid_new_leads = args$paid_new_leads
     paid_new_cost = args$paid_new_cost
+    multiply = args$multiply
     
     # return
     return(list(
         paid_new_campaign_list = paid_new_campaign_list,
         paid_new_clicks = paid_new_clicks,
         paid_new_leads = paid_new_leads,
-        paid_new_cost = paid_new_cost
+        paid_new_cost = paid_new_cost,
+        multiply = multiply
     ))
     
     
 }
 
-get_inputs()
+# get_inputs()
+
+
+# Get PPC Metrics ----
+get_ppc_metrics <-
+    function(data, campaign_name = NULL, campaign_level) {
+        
+        # grouping column setup
+        if (campaign_level == "campaign_total") {
+            group_var = "campaign"
+        } else if (campaign_level == "campaign_id") {
+            group_var = "campaign_id"
+        }
+        
+        # data setup
+        data <- data %>% as_tibble()
+        
+        # data filtering
+        if (is.null(campaign_name)) {
+            df <- data 
+        } else {
+            df <- data %>% 
+                filter(str_detect(str_to_lower(campaign), {{campaign_name}}))
+        }
+        
+        # aggregate calculation
+        df <- df %>% 
+            mutate(total = "Total") %>% 
+            select(total, {{group_var}}, cost, clicks, impressions, conversions) %>% 
+            pivot_longer(
+                cols = c(total, {{group_var}}),
+                names_to = "key", values_to = "campaign"
+            ) %>% 
+            summarise(
+                across(
+                    is.numeric,
+                    ~ sum(.x, na.rm = TRUE),
+                    .names = "total_{.col}"
+                ),
+                .by = campaign
+            ) %>% 
+            arrange(campaign)
+        
+        # factor reorder for sorting campaign column
+        if (df$campaign[1] == "AW_Apr17") {
+            desired_order <- c("AW_Apr17", "AW_May17", "AW_Jun17", "total")
+            df <- df %>% 
+                arrange(factor(campaign, levels = desired_order))
+        } else if (df$campaign[1] == "FB_Apr17") {
+            desired_order <- c("FB_Apr17", "FB_May17", "FB_Jun17", "total")
+            df <- df %>% 
+                arrange(factor(campaign, levels = desired_order))
+        }
+        
+        # metrics calculation
+        ret <- df %>% 
+            rename(total_leads = total_conversions) %>% 
+            mutate(
+                ctr = total_clicks / total_impressions,
+                cpc = total_cost / total_clicks,
+                ctl_cvr = total_leads / total_clicks
+            )
+        
+        # return
+        return(ret)
+        
+    }
--- apps/practice_apps/practice_app_1.R
+++ apps/practice_apps/practice_app_1.R
@@ -14,6 +14,7 @@ input_UI <- function(id) {
     textInput(ns("paid_new_clicks"), "Clicks", NULL),
     textInput(ns("paid_new_leads"), "Leads", NULL),
     textInput(ns("paid_new_cost"), "Cost", NULL),
+    textInput(ns("multiply"), "Multiply", NULL),
     hr(),
     actionButton(ns("apply"), "Apply")
   )
@@ -27,19 +28,24 @@ input_Server <- function(id) {
       # default inputs
       default_inputs <- reactive({get_inputs()})
       
+      # data
+      # campaign_tbl <- reactive({read_rds("practice_apps/data/campaign_data.rds")})
+      
       # update inputs with defaults
       observe({
         updateTextInput(session, "paid_new_campaign_list", value = paste(default_inputs()$paid_new_campaign_list, collapse = ", "))
         updateTextInput(session, "paid_new_clicks", value = paste(default_inputs()$paid_new_clicks, collapse = ", "))
         updateTextInput(session, "paid_new_leads", value = paste(default_inputs()$paid_new_leads, collapse = ", "))
         updateTextInput(session, "paid_new_cost", value = paste(default_inputs()$paid_new_cost, collapse = ", "))
+        updateTextInput(session, "multiply", value = paste(default_inputs()$multiply, collapse = ", "))
       })
       
       # apply button
       updated_inputs <- eventReactive(input[["apply"]], {
         list(
           campaign_list = stringr::str_split(input[["paid_new_campaign_list"]], ",")[[1]],
-          clicks = as.numeric(unlist(str_split(input[["paid_new_clicks"]], ",")))
+          clicks = as.numeric(unlist(str_split(input[["paid_new_clicks"]], ","))),
+          multiply = as.numeric(unlist(str_split(input[["multiply"]], ",")))
         
         )
       })
@@ -51,8 +57,10 @@ input_Server <- function(id) {
       output$calc <- renderTable({
         tibble(
           campaign = updated_inputs()$campaign_list,
-          clicks   = updated_inputs()$clicks
-        )
+          clicks   = updated_inputs()$clicks,
+          multiply = updated_inputs()$multiply
+        ) %>% 
+          mutate(res = clicks * multiply)
       })
       
       # return
@@ -71,14 +79,33 @@ combined_UI <- function(id) {
   )
 }
 
+data_UI <- function(id) {
+  ns <- NS(id)
+  tableOutput(ns("campaign_data"))
+}
+
+
 combined_Server <- function(id, updated_inputs) {
   moduleServer(
     id,
     function(input, output, session) {
+      
+      # data
+      campaign_tbl <- reactive({read_rds("data/campaign_data.rds")})
+      
+      # calculation table
       output$calculation_table <- renderTable({
         tibble(
           campaign = updated_inputs()$campaign_list,
           clicks   = updated_inputs()$clicks
+        )
+      })
+      
+      # calculation from data
+      output$campaign_data <- renderTable({
+        # campaign_tbl() %>% head()
+        get_ppc_metrics(
+          campaign_tbl(), NULL, "campaign_total"
         )
       })
       
@@ -104,7 +131,10 @@ ui <- fluidPage(
       tableOutput("input-calc"),
       hr(),
       p("Calculations"),
-      combined_UI("combined")
+      combined_UI("combined"),
+      hr(),
+      p("Calculations From Data:"),
+      data_UI("campaign_data")
     )
   )
 )
@@ -112,12 +142,9 @@ server <- function(input, output, session) {
   
   updated_inputs <- input_Server("input")
   
-  combined_Server(
-    id            = "combined",
-    # campaign_list = updated_inputs()$campaign_list,
-    # clicks        = updated_inputs()$clicks
-    updated_inputs = updated_inputs
-  )
+  combined_Server("combined", updated_inputs)
+  
+  combined_Server("campaign_data", updated_inputs)
 }
 
 shinyApp(ui, server)
